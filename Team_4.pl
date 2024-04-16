2d8ad82b22100668806320067d1d9bb2261d25cf
unknown <theoneandonlyzestro@gmail.com>
2024-04-17 00:21:24 +0200

    New Branch for Connection adjustment

--- Team_4.pl
+++ Team_4.pl
@@ -2,16 +2,19 @@
 % instead of needing to consult the files individually.
 % This line MUST be included in the final submission.
 :- ['transport_kb', 'slots_kb'].
-proper_connection(Station_A, Station_B, Duration, Line):-
-    connection(Station_A, Station_B, Duration, Line)
-    ;
-    (\+unidirectional(Line),
-    connection(Station_B, Station_A, Duration, Line)).
-proper_connection(Station_A, Station_B, Duration, Line):-
-    ((connection(Station_A, Station_C, Duration1, Line),
-    proper_connection(Station_C, Station_B, Duration2, Line))
-    ;
-    (\+unidirectional(Line),
-    connection(Station_B, Station_D, Duration1, Line),
-    proper_connection(Station_D, Station_A, Duration2, Line))),
-    Duration is Duration1+Duration2.
+
+proper_connection_default(A, B, D, L):- 
+    connection(A,B, D,L).
+
+proper_connection_default(A,B, D,L):-
+    \+connection(A,B,D,L),
+    connection(A,C,D1,L),
+    proper_connection_default(C,B,D2,L),
+    D is D1 + D2.
+    
+proper_connection(A, B, D, L):-
+    proper_connection_default(A, B, D, L).
+
+proper_connection(A, B, D, L):-
+    \+unidirectional(L),
+    proper_connection_default(B, A, D, L).
